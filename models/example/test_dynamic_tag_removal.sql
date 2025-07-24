d4e4276590a43d1ba0cbe418c1e7d681366cf672
Grace Seda <grace.seda@fishtownanalytics.com>
2025-07-24 02:16:48 +0000

    dvd

--- models/example/test_dynamic_tag_removal.sql
+++ models/example/test_dynamic_tag_removal.sql
@@ -2,7 +2,8 @@
     alias='test_asset',
     materialized="dynamic_table",
     target_lag="15 minutes",
-    snowflake_warehouse="MY_WH"
+    snowflake_warehouse="MY_WH",
+    tags=["dynamic_table"]
 ) }}
 
 select
