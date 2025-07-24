19748259c3f6fa8541aff3c005110dbe2d264eff
Grace Seda <grace.seda@fishtownanalytics.com>
2025-07-24 02:14:55 +0000

    sdgg

---
+++ models/example/test2_dynamic_tag_removal.sql
@@ -0,0 +1,12 @@
+{{ config(
+    alias='test2_asset',
+    materialized="dynamic_table",
+    target_lag="15 minutes",
+    snowflake_warehouse="MY_WH",
+    tags=["dynamic_table"]
+) }}
+
+select
+    *
+from {{ ref('my_first_dbt_model') }}  -- This should exist in your project
+limit 10
